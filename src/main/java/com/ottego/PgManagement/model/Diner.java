c1918f52ea171de3805c25709e86d6778aef822b
akash <akaashbx@gmail.com>
2024-08-27 19:39:46 +0530

    1

--- src/main/java/com/ottego/PgManagement/model/Diner.java
+++ src/main/java/com/ottego/PgManagement/model/Diner.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "diners")
+@Table(name = "diners", uniqueConstraints = {@UniqueConstraint(columnNames = {"stay_id", "meal_id"})})
 public class Diner {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
